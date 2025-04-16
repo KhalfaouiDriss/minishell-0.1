1a733f6bbcb77519ec5270bf55c7dc98cef5698b
C47 <drisspaca4@gmail.com>
2025-04-16 15:09:49 +0100

    minishell

--- include/minishell.h
+++ include/minishell.h
@@ -31,7 +31,6 @@ typedef struct s_token {
     int type;
     struct s_token *next;
 } t_token;
-//hhhhhhhhhhhhhhhhhhhhh
 
 typedef struct s_shell
 {
@@ -39,8 +38,8 @@ typedef struct s_shell
     int arg_count;
     char *input;
     t_token *token;
+    char *hhh;
 } t_shell;
 
 
-// kjhkhkhhk
 #endif
