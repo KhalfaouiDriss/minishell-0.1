f4f7db0c82fdbd9bf835c15d3e3ed464d9bbf575
C47 <drisspaca4@gmail.com>
2025-04-16 15:04:08 +0100

    minishell

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
 #include "minishell.h"
 
-int main()
+nt main(int ac, char **av, char **env)
 {
+    t_shell shell;
+
+    shell.env = NULL;
     
+    init_env(&shell, env);
+    
+    signal(SIGINT, get_sig);
+    signal(SIGQUIT, SIG_IGN);
+    while (1)
+    {
+        shell.input = readline("minishell$ ");
+        if (shell.input == NULL)
+        {
+            free_all(&shell);
+            printf("exit\n");
+            exit(1);
+        }
+        add_history(shell.input);
+        shell.args = simple_cmd(&shell, env);
+    }
+    return 0;
 }
