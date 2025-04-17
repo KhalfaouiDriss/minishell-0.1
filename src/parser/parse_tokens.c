2749a45f45f91d2ea65088ae03cd4bfde8a373e6
sel-bech <drisspaca4@gmail.com>
2025-04-17 16:46:37 +0100

    minishell

--- src/parser/parse_tokens.c
+++ src/parser/parse_tokens.c
@@ -13,20 +13,70 @@ int count_args(t_token *token)
     return count;
 }
 
-char *safe_strdup(const char *s) {
-    if (!s) return NULL;
+char *safe_strdup(const char *s)
+{
+    if (!s) 
+        return NULL;
     return ft_strdup(s);
 }
 
+void free_tokens(t_token *tokens)
+{
+    t_token *current;
+    t_token *next;
+    
+    current = tokens;
+    while (current)
+    {
+        next = current->next;
+        free(current->value);
+        free(current);
+        current = next;
+    }
+}
 
-t_cmd *parse_tokens(t_token *token) {
+void free_cmds(t_cmd *cmds)
+{
+    t_cmd *current;
+    t_cmd *next;
+    
+    current = cmds;
+    while (current)
+    {
+        next = current->next;
+        int i = 0;
+        while (current->args[i])
+            free(current->args[i++]);
+        free(current->args);
+        free(current->infile);
+        free(current->outfile);
+        free(current);
+        current = next;
+    }
+}
+
+t_cmd *parse_tokens(t_token *token)
+{
     t_cmd *head = NULL;
     t_cmd *last = NULL;
 
     while (token) {
-        t_cmd *cmd = ft_calloc(1, sizeof(t_cmd));
-        int argc = count_args(token);
-        cmd->args = ft_calloc(argc + 1, sizeof(char*));
+        t_cmd *cmd = malloc(sizeof(t_cmd));
+        if (!cmd)
+            return NULL;
+            
+        cmd->infile = NULL;
+        cmd->outfile = NULL;
+        cmd->next = NULL;
+        cmd->heredoc = 0;
+        cmd->append = 0;
+        
+        int c = count_args(token);
+        cmd->args = malloc((c + 1) * sizeof(char*));
+        if (!cmd->args) {
+            free(cmd);
+            return NULL;
+        }
 
         int i = 0;
         while (token && token->type != PIPE) {
