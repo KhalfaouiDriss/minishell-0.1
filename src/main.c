c8a380601e5ea24536ae40cbb16200ff3ac98be8
Saad El Bechna <drisspaca4@gmail.com>
2025-04-26 16:14:01 +0100

    minishell

--- src/main.c
+++ src/main.c
@@ -8,9 +8,9 @@ void	get_sig(int sig)
 		return ;
 	if (sig == SIGINT)
 	{
-		write(1, "\n", 1);
-		rl_on_new_line();
 		rl_replace_line("", 0);
+		rl_on_new_line();
+		write(1, "\n", 1);
 		rl_redisplay();
 	}
 }
