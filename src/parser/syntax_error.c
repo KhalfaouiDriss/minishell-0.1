61052412922d9d615ba90a8620b3015bb1b0f08f
driss <driss@example.com>
2025-07-12 17:43:36 +0100

    minishell

--- src/parser/syntax_error.c
+++ src/parser/syntax_error.c
@@ -3,18 +3,10 @@
 int is_operator(char *str, int i)
 {
 	if (str[i] == '<')
-	{
-		if (str[i + 1] == '<')
-			return 2;
-		return 1;
-	}
-	else if (str[i] == '>')
-	{
-		if (str[i + 1] == '>')
-			return 2;
-		return 1;
-	}
-	else if (str[i] == '|')
+		return (str[i + 1] == '<') ? 2 : 1;
+	if (str[i] == '>')
+		return (str[i + 1] == '>') ? 2 : 1;
+	if (str[i] == '|')
 		return 1;
 	return 0;
 }
@@ -28,41 +20,31 @@ int skip_spaces(char *str, int i)
 
 int check_syntax_errors(t_shell *shell)
 {
-	int i = 0, op_len, last_was_op = 1;
+	int i = 0, op_len;
 	char *input = shell->input;
 
 	i = skip_spaces(input, 0);
 	if (!input[i])
-		return 0; 
-	i = 0;
+		return 0; // empty input is OK
+
 	while (input[i])
 	{
 		i = skip_spaces(input, i);
-		if (!input[i])
-			break;
-
 		op_len = is_operator(input, i);
 		if (op_len)
 		{
-			if (last_was_op) {
-				printf("syntax error near unexpected token '%.*s'\n", op_len, &input[i]);
+			i += op_len;
+			i = skip_spaces(input, i);
+			if (!input[i] || is_operator(input, i)) {
+				printf("syntax error near unexpected token '%.*s'\n", op_len, &input[i - op_len]);
 				return 1;
 			}
-			last_was_op = 1;
-			i += op_len;
 		}
 		else
 		{
 			while (input[i] && !isspace(input[i]) && !is_operator(input, i))
 				i++;
-			last_was_op = 0;
 		}
-	}
-
-	if (last_was_op) {
-		printf("syntax error: unexpected end of input after operator\n");
-		return 1;
 	}
 	return 0;
 }
-
