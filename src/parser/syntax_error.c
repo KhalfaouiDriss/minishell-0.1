7b73604a6c103fab8c072ef08c77c3bea7475f1b
driss <driss@example.com>
2025-07-14 16:51:13 +0100

    minishell

--- src/parser/syntax_error.c
+++ src/parser/syntax_error.c
@@ -6,7 +6,7 @@
 /*   By: khalfaoui47 <khalfaoui47@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 17:18:37 by dkhalfao          #+#    #+#             */
-/*   Updated: 2025/07/14 16:49:45 by khalfaoui47      ###   ########.fr       */
+/*   Updated: 2025/07/14 16:50:55 by khalfaoui47      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ static int	handle_unmatched_quote(char quote, char *input, int *i,
 		(*i)++;
 	if (!input[*i])
 	{
-		if(shell->pos = 1)
+		if(shell->pos == 1)
 		{
 			printf("syntax error: unmatched quote\n");
 			shell->exit_status = 130;
@@ -65,9 +65,9 @@ static int	handle_operator(char *input, int *i, t_shell *shell)
 		if (!input[*i] || input[0] == '|' || (is_operator(input, *i)
 				&& !((input[*i] == '<') || (input[*i] == '>'))))
 		{
-			if(shell->pos = 1)
+			if(shell->pos == 1)
 			{
-				printf("syntax error\n");
+				printf("syntax error \n");
 				shell->exit_status = 2;
 			}
 			return (1);
